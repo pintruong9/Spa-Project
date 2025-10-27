f65619475acd3c4f37022b666b03fa3b2618d165
pintruong <pintruong.work@gmail.com>
2025-10-27 22:48:52 +0700

    Initial commit

---
+++ backend/bby_spa.session.sql
@@ -0,0 +1,31 @@
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(100) NOT NULL,
+    phone VARCHAR(20) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE,
+    password TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+
+CREATE TABLE spas (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(100) NOT NULL,
+    address TEXT NOT NULL,
+    phone VARCHAR(20),
+    description TEXT,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE services (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    id_spa UUID REFERENCES spas(id) ON DELETE CASCADE,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    duration_minutes INT,
+    price NUMERIC(10,2),
+    image_url TEXT,
+    is_active BOOLEAN DEFAULT TRUE
+);
+
+s
